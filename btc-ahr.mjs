a8f968e964d7c07e45d72a07b19d757ca687acf1
Jelly Cao <zheng.cao@iherb.com>
2024-08-31 20:12:06 +0800

    chore: debugger.

---
+++ btc-ahr.mjs
@@ -0,0 +1,58 @@
+import nodeCron from "node-cron";
+
+const userIDs = [
+  "UID_9060m1uKN8xNGvPbuqvxxOMQYwB2",
+  "UID_xbFfSwmHfKcRCcRdMTOjFlWix0pk",
+];
+
+const ahrFetch = async () => {
+  const btcAhr999 = await fetch(
+    "https://dncapi.shermanantitrustact.com/api/v2/index/arh999?code=bitcoin&webp=1"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data?.code !== 200) return null;
+
+      if (Array.isArray(data.data) && data.data.length > 0) {
+        const latestData = data.data.at(-1);
+
+        return {
+          ahr999: latestData[1],
+          btc: latestData[2],
+        };
+      }
+
+      return null;
+    })
+    .catch(() => null);
+
+  if (btcAhr999) {
+    await fetch("https://wxpusher.zjiecode.com/api/send/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        appToken: process.env.app_token,
+        content: JSON.stringify(btcAhr999),
+        contentType: 1,
+        summary: `BTC-${btcAhr999.btc}-AHR-${btcAhr999.ahr999}`,
+        uids: userIDs,
+        verifyPayType: 0,
+      }),
+    })
+      .then((response) => response.json())
+      .then(console.log)
+      .catch((error) => {
+        console.log("推送信息错误:", error);
+      });
+  }
+};
+
+const task = nodeCron.schedule("*/2 * * * *", () => {
+  (async () => {
+    await ahrFetch();
+  })();
+});
+
+task.start();
